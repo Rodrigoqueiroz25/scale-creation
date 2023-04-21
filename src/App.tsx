7f2ce081e18b07112f2575899b560f092d6b6743
Rodrigoqueiroz25 <digoqueiroz19@gmail.com>
2023-04-20 21:54:58 -0300

    feat: created components Header and Footer
    Components Header and Footer created for all aplication.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <Header/>
+      
+      <Footer/>
     </div>
   );
 }
