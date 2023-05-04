79f6d07f441a3f0a31d2d0a1aa346a196004b059
Rodrigoqueiroz25 <digoqueiroz19@gmail.com>
2023-05-03 21:57:37 -0300

    feat(home): "Nova Escala" button redirecting to the SelecaoMes screen

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,7 +10,7 @@ export function Home(){
         <h2>Bem Vindo</h2>
 
             <div className={styles.cardContainer}>
-              <Link className={styles.card} to="">
+              <Link className={styles.card} to="/selecao-mes">
                 <svg className={styles.materialIcons} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
                 <span>Nova Escala</span>
               </Link>
