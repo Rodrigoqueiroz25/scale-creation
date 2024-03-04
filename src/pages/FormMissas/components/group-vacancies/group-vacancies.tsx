fd6ca7475edfb1daa1114bd24dc772c64758b301
Rodrigoqueiroz25 <digoqueiroz19@gmail.com>
2024-03-04 11:14:04 -0300

    feat(group-vacancies): create buttons plus and less

--- src/pages/FormMissas/components/group-vacancies/group-vacancies.tsx
+++ src/pages/FormMissas/components/group-vacancies/group-vacancies.tsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import { Vacancy } from '../vacancy/vacancy';
 import styles from './style.module.css';
 
@@ -8,9 +9,11 @@ type Props = {
 
 export function GroupVacancies({ numVacancies }: Props) {
 
+    const [vacancies, setVacancies] = useState<number>(numVacancies);
+
     const renderSelects = () => {
         const selects = [];
-        for (let index = 0; index < numVacancies; index++) {
+        for (let index = 0; index < vacancies; index++) {
             selects.push(
                 <Vacancy key={index} num={index}/>
             )
@@ -18,7 +21,23 @@ export function GroupVacancies({ numVacancies }: Props) {
         return selects;
     }
 
+    function handleClickPlus(){
+        setVacancies(vacancies + 1);
+    }
+
+    function handleClickLess(){
+        if(vacancies > 2){
+            setVacancies(vacancies - 1);
+        }
+    }
+
     return (
-        <div className={styles.selects}>{renderSelects()}</div>
+        <div className={styles.container}>
+            <div className={styles.selects}>{renderSelects()}</div>
+            <div className={styles.buttons}>
+                <button type='button' onClick={handleClickPlus}>+</button>
+                <button type='button' onClick={handleClickLess}>-</button>
+            </div>
+        </div>
     );
 }
